a0f1e299bab66c70f91b5386978d55f3265ed36f
latifahg81 <latifahg81@gmail.com>
2026-09-04 18:19:54 -0400

    Update Part1.py

--- Part1.py
+++ Part1.py
@@ -1,9 +1,10 @@
-Part 1 
 
 String1 = 'John Doe, 999-99-9999, 2-23-2003: 19.8973' #stores info
 
-colon_pos = String1.find(':') #
-extracted = String1[colon_pos + 1:].strip() #takes everything after :
-number = float(extracted) #converts the number 19.8973 from string into a floating point number 
+colon_pos = String1.find(':') #finds the position of the colon
 
-print number 
+extracted = String1[colon_pos + 1:].strip() #takes everything after :
+
+number = float(extracted) #converts the number 19.8973 from string into a floating point number
+
+print(number) #prints the floating point number
